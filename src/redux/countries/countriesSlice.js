65784497c8d29d557b99f59c8052bc3369daaa69
forLoop94 <charlesuwem94@gmail.com>
2023-06-27 12:16:06 +0100

    Fetch countries from Rest Countries API

--- src/redux/countries/countriesSlice.js
+++ src/redux/countries/countriesSlice.js
@@ -1,37 +1,53 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
-const mockCountries = [
-  {
-    name: 'Nigeria',
-    flag: 'green-white',
-    population: 207000000,
-    region: 'west-africa',
-    id: 1,
-  },
-  {
-    name: 'Ghana',
-    flag: 'black-gold',
-    population: 67000000,
-    region: 'west-africa',
-    id: 2,
-  },
-  {
-    name: 'Senegal',
-    flag: 'green-orange',
-    population: 87000000,
-    region: 'west-africa',
-    id: 3,
-  },
-];
+// const mockCountries = [
+//   {
+//     name: 'Nigeria',
+//     flag: 'green-white',
+//     population: 207000000,
+//     region: 'west-africa',
+//     id: 1,
+//   },
+//   {
+//     name: 'Ghana',
+//     flag: 'black-gold',
+//     population: 67000000,
+//     region: 'west-africa',
+//     id: 2,
+//   },
+//   {
+//     name: 'Senegal',
+//     flag: 'green-orange',
+//     population: 87000000,
+//     region: 'west-africa',
+//     id: 3,
+//   },
+// ];
 
 const initialState = {
-  countriesData: mockCountries,
+  countriesData: [],
 };
+
+const DISPLAYCOUNTRIES = 'population-metrics-app/redux/countries/countriesSlice/DISPLAYCOUNTRY';
+
+const URL = 'https://restcountries.com/v3.1/region/africa';
+
+const fetchCountries = createAsyncThunk(DISPLAYCOUNTRIES, async () => {
+  const response = await axios.get(URL);
+  const result = response.data;
+  return result;
+});
 
 const countriesSlice = createSlice({
   name: 'countries',
   initialState,
   reducers: {},
+  extraReducers: (build) => {
+    build.addCase(fetchCountries.fulfilled, (state, action) => {
+      state.countriesData = action.payload;
+    });
+  },
 });
 
 export default countriesSlice.reducer;
