6fa051b00f3bd41d82f1cec58b586d5ffb51a01e
forLoop94 <charlesuwem94@gmail.com>
2023-06-27 09:16:32 +0100

    Add Navbar component to provide a chevron left icon that links back to the homepage

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Countries from './components/Countries';
 import './App.css';
 import CountryDetails from './components/CountryDetails';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Countries />} />
-        <Route path="/country" element={<CountryDetails />} />
-      </Routes>
+      <div>
+        <Navbar>Most Views</Navbar>
+        <Routes>
+          <Route path="/" element={<Countries />} />
+          <Route
+            path="/country"
+            element={<CountryDetails />}
+          />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { PropTypes } from 'prop-types';
+import { FaChevronLeft } from 'react-icons/fa';
+import { FaMicrophone } from 'react-icons/fa6';
+import { Link } from 'react-router-dom';
+
+const Navbar = (props) => {
+  const { children } = props;
+  return (
+    <header>
+      <Link to="/">
+        <FaChevronLeft color="blue" fontSize="12px" />
+      </Link>
+      <div>{children}</div>
+      <div>
+        <FaMicrophone color="blue" fontSize="12px" />
+      </div>
+    </header>
+  );
+};
+
+Navbar.propTypes = {
+  children: PropTypes.string.isRequired,
+};
+
+export default Navbar;
