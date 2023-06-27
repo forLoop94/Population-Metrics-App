fd01b2f11200696264104d37490e235130ed5f8a
forLoop94 <charlesuwem94@gmail.com>
2023-06-27 19:12:57 +0100

    Dispatch fetchCountry action to fetch target country when user types country name in search input

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
 import './App.css';
 import CountryDetails from './components/CountryDetails';
 import Navbar from './components/Navbar';
-import { fetchCountries } from './redux/countries/countriesSlice';
 import Home from './components/Home';
 
 function App() {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(fetchCountries());
-  }, [dispatch]);
-
   return (
     <BrowserRouter>
       <div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,31 @@
-import React, { useState } from 'react';
+// eslint-disable-next-line react-hooks/exhaustive-deps
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchCountries, fetchCountry } from '../redux/countries/countriesSlice';
+import Countries from './Countries';
 
 const Home = () => {
   const [countryName, searchCountryName] = useState('');
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCountries());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (countryName !== '') {
+      setTimeout(() => {
+        dispatch(fetchCountry(countryName));
+      }, 1000);
+    } else {
+      dispatch(fetchCountries());
+    }
+  }, [countryName]);
+
+  const handleClick = () => {
+    window.location.reload();
+  };
+
   return (
     <div>
       <h2>Africa</h2>
@@ -12,6 +36,8 @@ const Home = () => {
         value={countryName}
         onChange={(e) => searchCountryName(e.target.value)}
       />
+      <button type="button" onClick={handleClick}>Clear</button>
+      <Countries />
     </div>
   );
 };
